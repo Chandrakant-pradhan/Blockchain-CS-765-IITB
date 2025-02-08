d70fcec3db1c334f05545175dd222ea84630960c
NimishManware <22b0944@iitb.ac.in>
2025-02-08 13:27:58 +0530

    completed peer functionalities

--- src/event.cpp
+++ src/event.cpp
@@ -1,8 +1,76 @@
-#include "event.hpp"
-#include "peer.hpp"
+#include "defs.hpp"
 
-void CreateTxnEvent::execute(){
-    Peer * owner = this->peer;
-    peer->createTxn(globalTime);
-    peer->runTxn();
+Event :: Event(ld timestamp) {
+    this->executeTime = timestamp;
+}
+
+Tell_node_to_create_txn::Tell_node_to_create_txn(Peer* node, ld curr_time) : Event(0), node(node) {
+    static default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+    exponential_distribution<double> exp_dist(1.0 / node->sim->Ttx);
+    this->executeTime = curr_time + exp_dist(generator);
+}
+
+Tell_node_to_create_block::Tell_node_to_create_block(Peer* node, ld curr_time) : Event(0), node(node) {
+    static std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
+    std::exponential_distribution<double> exp_dist(node->hk / node->sim->I);
+    this->executeTime = curr_time + exp_dist(generator);
+    cout<<"Order Blk creation "<<node->ID<<" "<<curr_time<<endl;
+}
+
+
+Tell_node_they_rcvd_txn::Tell_node_they_rcvd_txn(Peer* node, Transaction* txn, ld curr_time) : Event(0), node(node), txn(txn) {
+    this->executeTime = curr_time;
+}
+
+Tell_node_they_rcvd_block::Tell_node_they_rcvd_block(Peer* node, Block* block, ld curr_time) : Event(0), node(node), block(block) {
+    this->executeTime = curr_time;
+}
+
+void Tell_node_to_create_txn::execute(){
+    
+    if(node->balance[node->ID] > 0){
+        Transaction* txn = node->on_order_to_create_txn(this->executeTime);
+        node->txn_pool.insert(txn);
+        for (Peer* neigh : node->neigh) {
+            Link* link = node->link_map[neigh];
+            ld delay = link->getTotalDelay(txn->size);
+            Tell_node_they_rcvd_txn* event = new Tell_node_they_rcvd_txn(neigh, txn->make_copy(), this->executeTime + delay);
+            node->sim->event_queue.push(event);
+        }
+    }
+    node->sim->event_queue.push(new Tell_node_to_create_txn(node, this->executeTime));
+}
+
+void Tell_node_they_rcvd_txn::execute(){
+    node->on_receive_txn(txn);
+    for (Peer* neigh : node->neigh) {
+        if(node->sent_map_txn[txn->ID].find(neigh) == node->sent_map_txn[txn->ID].end() &&
+           neigh->sent_map_txn[txn->ID].find(node) == neigh->sent_map_txn[txn->ID].end()){
+            Link* link = node->link_map[neigh];
+            ld delay = link->getTotalDelay(txn->size);
+            Event* event = new Tell_node_they_rcvd_txn(neigh, txn->make_copy(), this->executeTime + delay);
+            node->sent_map_txn[txn->ID].insert(neigh);
+            node->sim->event_queue.push(event);
+        }
+    }
+} 
+
+void Tell_node_to_create_block::execute(){
+    if(!isStillValid) return;
+    node->on_order_to_create_block(this->executeTime);
+}
+
+void Tell_node_they_rcvd_block::execute(){
+    cout<<"RECEIVED/////*/\n";
+    node->on_receive_block(block);
+    for (Peer* neigh : node->neigh) {
+        if(node->sent_map_block[block->ID].find(neigh) == node->sent_map_block[block->ID].end() &&
+           neigh->sent_map_block[block->ID].find(node) == neigh->sent_map_block[block->ID].end()){
+            Link* link = node->link_map[neigh];
+            ld delay = link->getTotalDelay(block->size);
+            Event* event = new Tell_node_they_rcvd_block(neigh, block, this->executeTime + delay);
+            node->sent_map_block[block->ID].insert(neigh);
+            node->sim->event_queue.push(event);
+        }
+    }
 }
--- src/peer.cpp
+++ src/peer.cpp
@@ -1,62 +1,226 @@
-#include <iostream>
-#include "headers.hpp"
-#include "transaction.hpp"
-#include "link.hpp"
-#include "peer.hpp"
-#include "event.hpp"
+#include "defs.hpp"
 
-// Add a link to neighbour
-void Peer::addLink(Link* lk){
-    if(links.size() > 6) return; // Not more than 6 neighbours
+Peer::Peer(Simulation * sim, int ID, bool slow, bool low) : sim(sim), ID(ID), slow(slow), low(low) {
+    geneisLocal = sim->global_genesis_block->make_copy_for_peer(this);
+    longest_chain_tips.push_back(sim->global_genesis_block);
+    block_map[sim->global_genesis_block->ID] = sim->global_genesis_block;
+}
 
-    auto it = find(neighbour_peers.begin(), neighbour_peers.end(),lk->other);
-    if(it == neighbour_peers.end()) return; // Repeated link between two peers
+Peer* Peer::get_random_peer() {
+    vector<Peer*> eligible_peers;
+    for (const auto& entry : sim->peer_map) {
+        if (entry.second != this) { 
+            eligible_peers.push_back(entry.second);
+        }
+    }
+    if (eligible_peers.empty()) return nullptr;
+    
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dist(0, eligible_peers.size() - 1);
+    return eligible_peers[dist(gen)];
+}
 
-    neighbour_peers.push_back(lk->other);
-    links.push_back(lk);
+int Peer::get_random_money() {
+    // balance in the self ID
+    int max_money = balance[ID];
+    if (max_money <= 0) return 0;
+    
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dist(1, max_money);
+    return dist(gen);
+}
+
+Transaction* Peer::on_order_to_create_txn(ld timestamp) {
+    Peer* receiver = get_random_peer();
+    if (!receiver) return nullptr;
+    int amount = get_random_money();
+    if (amount == 0) return nullptr;
+    Transaction* txn = new Transaction(timestamp, this->ID, receiver->ID, amount);
+    txn_map[txn->ID] = txn;  // Store in map for reference
+    return txn;
+}
+
+void Peer::on_receive_txn(Transaction* txn) {
+    txn_pool.insert(txn);
+    // TODO: add the logic here instead of in the event execute
+}
+
+void Peer::on_order_to_create_block(ld timestamp) {
+    vector<Transaction*> txns_to_include;
+    auto txn = txn_pool.begin();
+
+    // Find txns to include in block;
+    while (txn != txn_pool.end() && txns_to_include.size() <= (sim->max_block_size / sim->transaction_size - 1)) {
+        if(balance[(*txn)->senderID] > (*txn)->amount) // Amount exceeds balance
+                {   txn++;
+                    continue;
+                }
+        txns_to_include.push_back(*txn);
+    }
+
+    // Update the balances and delete the txns from pool
+    for(auto txn : txns_to_include){
+        balance[txn->senderID] -= txn->amount;
+        balance[txn->receiverID] += txn->amount;
+        txn_pool.erase(txn);
+    }
+
+
+    Block* parent = miningBlock;
+    // next it has pointer to parent and current block of miner and all the transactions to include
+    // there is in blockchain that
+    Block* block = new Block(timestamp, parent, this, txns_to_include);
+    miningBlock = block;
+    balance[ID] += 50;
+
+    Tell_node_to_create_block* event = new Tell_node_to_create_block(this,sim->global_time);
+    sim->event_queue.push(event);
+
+    // Now send to all the neighbours
+    for(auto n : neigh){
+        int delay = link_map[n]->getTotalDelay(block->size);
+        Tell_node_they_rcvd_block * event = new Tell_node_they_rcvd_block(n,block,sim->global_time + delay);
+        sim->event_queue.push(event);
+        sent_map_block[block->ID].insert(n);
+    }
+
+}
+void Peer::change_branch(Block* newBlock){
+    // revert current chain's effect
+    Block* oldBranch= miningBlock;
+    Block* newBranch = newBlock;
+    // Now the newBranch is ahead by one so just 
+    // move it back by one to reach LCA
+
+    while(newBranch != oldBranch){
+        newBranch = newBranch -> parentBlock;
+        oldBranch = newBranch -> parentBlock;
+    }
+    // Found LCA now just revert the Txns of new branch
+    while(miningBlock != oldBranch){
+        for(auto txn : miningBlock->txns){
+            balance[txn->senderID] += txn->amount;
+            balance[txn->receiverID] -= txn->amount;
+            txn_pool.insert(txn);
+        }
+        balance[newBlock -> miner -> ID] -= 50; // Mining Fee
+        miningBlock = miningBlock->parentBlock;
+    }
+
+
+    // Add the txns of new branch
+    miningBlock = newBlock; // Change current mining Block
+    while(oldBranch != newBlock){
+        for(auto txn : newBlock->txns){
+            balance[txn->senderID] -= txn->amount;
+            balance[txn->receiverID] += txn->amount;
+            txn_pool.erase(txn);
+        }
+        balance[newBlock -> miner -> ID] += 50; // Mining Fee
+        newBlock = newBlock->parentBlock;
+    }
+}
+
+void Peer::add_block_to_chain(Block* block) {
+    Block* newBlock = block->make_copy_for_peer(this);
+    
+    block_map[newBlock->ID] = newBlock;
+    block_chain.push_back(newBlock);
+
+    // If in original chain => add it as it is
+    if(block->parentBlock == miningBlock){
+        miningBlock = block;
+        balance[block->miner->ID] += 50; // Mining Fee
+
+        // initiate a new block creation event
+        Tell_node_to_create_block * event = new Tell_node_to_create_block(this,sim->global_time);
+        sim->event_queue.push(event);
+        // invalidate the ongoing creation process
+        currBlockProc->isStillValid = false;
+        currBlockProc = event;
+    }
+    else if(block->depth > miningBlock->depth){
+        longest_chain_tips.clear();
+        longest_chain_tips.push_back(block);
+
+        // Now handle the branch change
+        change_branch(block);
+
+        // initiate a new block creation event
+        Tell_node_to_create_block * event = new Tell_node_to_create_block(this,sim->global_time);
+        sim->event_queue.push(event);
+        // invalidate the ongoing creation process
+        currBlockProc->isStillValid = false;
+        currBlockProc = event;
+    }
+    else if(block->depth == miningBlock->depth){
+        longest_chain_tips.push_back(block);
+    }
 
 }
 
-// Validity of Txn
-bool Peer::checkTxn(Transaction * txn){
-    int sender = txn->senderID;
-    int receiver = txn->receiverID;
-    if(sender > last_id || receiver > last_id) return false;
-    if(balances[sender] < txn->amount) return false;
-    else return true;
-}
+void Peer::on_receive_block(Block* block) {
+    if (block_map.find(block->ID) != block_map.end()) return; // Block already known
 
-// Runs the creation of txn event
-void Peer::runTxn(){ 
-    ld sample_time = randomExp(Ttx);
-    CreateTxnEvent * event = new CreateTxnEvent(this, sample_time);
-}
+    int parentID = block->parentBlock->ID;
 
-// Create the actual Txn object
-void Peer::createTxn(ld ts){
-    int balance = balances[id];
-    int receiver = randomInt(1,last_id);
-    int amount = randomInt(0, balance); // Doubt : 0 should be allowed?? 
-    Transaction * txn = new Transaction(ts,id,receiver,amount);
-    pool.insert(txn);
-    broadcastTxn(txn);
-}
+    if(block_map.find(parentID) == block_map.end()){
+        // Parent not arrived just put it in pool
+        block_pool.insert(block);
+    }
+    else if(parentID == miningBlock->ID){
+        // In the main chain
+        for(auto txn : block->txns){
+            if(balance[txn->senderID] > txn->amount) // Amount exceeds balance
+                return;
+            // Actual change in balance happens when block gets added in the chain
+        }
+        for(auto txn : block->txns){
+            txn_pool.erase(txn);
+        }
+        for(auto txn : block->txns){
+            balance[txn->senderID] -= txn->amount;
+            balance[txn->receiverID] += txn->amount;
+        }
+        add_block_to_chain(block);
 
-// Receive the Txn block from neighbour
-void Peer::receiveTxn(Transaction* txn){
-    if(!checkTxn(txn)) return;
-
-}
-
-void Peer::broadcastTxn(Transaction * txn){
-    for(auto neigh : neighbour_peers){
-        if(true){ // Should have some check
-            int delay;
-            for(auto lk : links){
-                if(lk->other == neigh) 
-                    delay = lk->getTotalDelay(txn->size);
+        // Check if some orphaned block now has a parent.
+        for(auto orphan : block_pool){
+            if(orphan->parentBlock->ID == block->ID){
+                add_block_to_chain(orphan);
             }
-            ReceiveTxn * event = new ReceiveTxn(delay, neigh,txn);
+        }
+    }
+    else{
+        // Creating or adding to fork
+        for(auto txn : block->txns){
+            if(balance[txn->senderID] > txn->amount) // Amount exceeds balance
+                return;
+        }
+        // No Txn removal from pool as it is not in the main chain
+        add_block_to_chain(block);
+
+        // Check if some orphaned block now has a parent.
+        for(auto orphan : block_pool){
+            if(orphan->parentBlock->ID == block->ID){
+                add_block_to_chain(orphan);
+            }
+        }
+    }
+
+    Block* parent = block_map[parentID];
+    
+    for(auto n : neigh){
+        if((n->sent_map_block[block->ID].find(this) == n->sent_map_block[block->ID].end())
+        && (sent_map_block[block->ID].find(n) == sent_map_block[block->ID].end()))
+        {
+            // Send to neighbours and Add to sent map
+            int delay = link_map[n]->getTotalDelay(block->size);
+            Tell_node_they_rcvd_block * event = new Tell_node_they_rcvd_block(n,block,sim->global_time + delay);
+            sim->event_queue.push(event);
+            sent_map_block[block->ID].insert(n);
         }
     }
 }
